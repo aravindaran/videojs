9f9f16b7cdb8cb00acdaba8a36f5b6164351488d
Aravind <Aravind@gmail.com>
2017-02-16 18:02:47 +0530

    first

---
+++ videojsXBlock/videojs/static/js/videojs_view.js
@@ -0,0 +1,14 @@
+/* Javascript for videojsXBlock. */
+function videojsXBlockInitView(runtime, element) {
+	alert($('.valiantPhoto',element).Valiant360);
+	$('.valiantPhoto',element).Valiant360();
+    /* Weird behaviour :
+     * In the LMS, element is the DOM container.
+     * In the CMS, element is the jQuery object associated*
+     * So here I make sure element is the jQuery object */
+    // if(element.innerHTML) element = $(element);
+    
+    // var video = element.find('video:first');
+    // videojs(video.get(0), {playbackRates:[0.75,1,1.25,1.5,1.75,2]}, function() {});
+
+}
---
+++ videojsXBlock/videojs/videojs.py
@@ -0,0 +1,151 @@
+""" videojsXBlock main Python class"""
+
+import pkg_resources
+from django.template import Context, Template
+
+from xblock.core import XBlock
+from xblock.fields import Scope, Integer, String, Boolean
+from xblock.fragment import Fragment
+
+class videojsXBlock(XBlock):
+
+    '''
+    Icon of the XBlock. Values : [other (default), video, problem]
+    '''
+    icon_class = "video"
+
+    '''
+    Fields
+    '''
+    display_name = String(display_name="Display Name",
+        default="360 Degree Video",
+        scope=Scope.settings,
+        help="This name appears in the horizontal navigation at the top of the page.")
+
+    url = String(display_name="Video URL",
+        default="http://vjs.zencdn.net/v/oceans.mp4",
+        scope=Scope.content,
+        help="The URL for your video.")
+    
+    allow_download = Boolean(display_name="Video Download Allowed",
+        default=True,
+        scope=Scope.content,
+        help="Allow students to download this video.")
+    
+    source_text = String(display_name="Source document button text",
+        default="",
+        scope=Scope.content,
+        help="Add a download link for the source file of your video. Use it for example to provide the PowerPoint or PDF file used for this video.")
+    
+    source_url = String(display_name="Source document URL",
+        default="",
+        scope=Scope.content,
+        help="Add a download link for the source file of your video. Use it for example to provide the PowerPoint or PDF file used for this video.")
+    
+    start_time = String(display_name="Start time",
+        default="",
+        scope=Scope.content,
+        help="The start and end time of your video. Equivalent to 'video.mp4#t=startTime,endTime' in the url.")
+    
+    end_time = String(display_name="End time",
+        default="",
+        scope=Scope.content,
+        help="The start and end time of your video. Equivalent to 'video.mp4#t=startTime,endTime' in the url.")
+
+    '''
+    Util functions
+    '''
+    def load_resource(self, resource_path):
+        """
+        Gets the content of a resource
+        """
+        resource_content = pkg_resources.resource_string(__name__, resource_path)
+        return unicode(resource_content)
+
+    def render_template(self, template_path, context={}):
+        """
+        Evaluate a template by resource path, applying the provided context
+        """
+        template_str = self.load_resource(template_path)
+        return Template(template_str).render(Context(context))
+
+    '''
+    Main functions
+    '''
+    def student_view(self, context=None):
+        """
+        The primary view of the XBlock, shown to students
+        when viewing courses.
+        """
+        fullUrl = self.url
+        if self.start_time != "" and self.end_time != "":
+            fullUrl += "#t=" + self.start_time + "," + self.end_time
+        elif self.start_time != "":
+            fullUrl += "#t=" + self.start_time
+        elif self.end_time != "":
+            fullUrl += "#t=0," + self.end_time
+        
+        context = {
+            'display_name': self.display_name,
+            'url': self.runtime.local_resource_url(
+                self, 'public/videos/overpass-clip.mp4'),
+            'allow_download': self.allow_download,
+            'source_text': self.source_text,
+            'source_url': self.source_url,
+        }
+        print fullUrl
+        html = self.render_template('static/html/videojs_view.html', context)
+        
+        frag = Fragment(html)
+        frag.add_css(self.load_resource("static/css/video-js.min.css"))
+        frag.add_css(self.load_resource("static/css/videojs.css"))
+        frag.add_css(self.load_resource("static/css/valiant360.css"))
+        frag.add_javascript(self.load_resource("static/js/three.min.js"))
+        frag.add_javascript(self.load_resource("static/js/jquery.valiant360.js"))
+        frag.add_javascript(self.load_resource("static/js/video-js.js"))
+        frag.add_javascript(self.load_resource("static/js/videojs_view.js"))
+        frag.initialize_js('videojsXBlockInitView')
+        return frag
+
+    def studio_view(self, context=None):
+        """
+        The secondary view of the XBlock, shown to teachers
+        when editing the XBlock.
+        """
+        context = {
+            'display_name': self.display_name,
+            'url': self.url,
+            'allow_download': self.allow_download,
+            'source_text': self.source_text,
+            'source_url': self.source_url,
+            'start_time': self.start_time,
+            'end_time': self.end_time
+        }
+        html = self.render_template('static/html/videojs_edit.html', context)
+        
+        frag = Fragment(html)
+        frag.add_javascript(self.load_resource("static/js/videojs_edit.js"))
+        frag.initialize_js('videojsXBlockInitStudio')
+        return frag
+
+    @XBlock.json_handler
+    def save_videojs(self, data, suffix=''):
+        """
+        The saving handler.
+        """
+        self.display_name = data['display_name']
+        self.url = data['url']
+        self.allow_download = True if data['allow_download'] == "True" else False # Str to Bool translation
+        self.source_text = data['source_text']
+        self.source_url = data['source_url']
+        self.start_time = ''.join(data['start_time'].split()) # Remove whitespace
+        self.end_time = ''.join(data['end_time'].split()) # Remove whitespace
+        
+        return {
+            'result': 'success',
+        }
+
+    @staticmethod
+    def workbench_scenarios():
+        """A canned scenario for display in the workbench."""
+        return [("VideoJs", "<vertical_demo><videojs/></vertical_demo>")]
